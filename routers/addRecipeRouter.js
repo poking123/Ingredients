e0f081ba37707851b7da4af6a1c9a74642d22f86
Alec Rodgers <alecr1997@gmail.com>
2019-02-05 19:19:44 -0500

    Refactor controller logic into routers

--- routers/addRecipeRouter.js
+++ routers/addRecipeRouter.js
@@ -1,19 +1,61 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
+var ingredientModel = require('../models/ingredientModel');
+var pluralize = require('../lib/pluralize');
 
-var addRecipeController = require('../controllers/addRecipeController');
+var ingredientsList = [];
 
 // Add_Recipe Page
-router.get('/', addRecipeController.addRecipePage);
+router.get('/', function(req, res) {
+  res.render('addRecipe', {
+      ingredientsList: ingredientsList
+  });
+});
 
 // Add Ingredient
-router.post('/ingredient/add', addRecipeController.addIngredient);
+router.post('/ingredient/add', function(req, res) {
+  var ingredientName = req.body.name;
+  var quantity = req.body.quantity;
+  
+  ingredientName = pluralize.ingredientPlurality(ingredientName, quantity);
+  
+  var ingredient = {
+      name: ingredientName,
+      quantity: quantity
+  };
+      
+  ingredientsList.push(ingredient);
+  res.send(ingredient);
+  //res.send(ingredientsList);
+});
 
 // Delete ingredient
-router.delete('/ingredient/delete/:name', addRecipeController.deleteIngredient);
+router.delete('/ingredient/delete/:name', function(req, res) {
+  var ingredientName = req.params.name;
+  ingredientsList = ingredientsList.filter(function(ingredient) {
+      return ingredient.name.replace(/ /g, '-') !== ingredientName;
+  });
+  res.send(ingredientsList);
+});
 
 // Add recipe
-router.post('/recipe/add', addRecipeController.addRecipe);
+router.post('/recipe/add', function(req, res) {
+  // clears the ingredients on the list
+  ingredientsList = [];
+  
+  var ingredients = req.body.ingredients;
+  for (var i = 0; i < ingredients.length; i++) {
+      var ingredient = ingredients[i];
+      var quantity = ingredient.quantity;
+      ingredient.name = pluralize.ingredientPlurality(ingredient.name, quantity);
+  }
+  
+  var recipe = new ingredientModel({
+      name: req.body.name,
+      ingredients: ingredients
+  });
+  recipe.save();
+  res.send(recipe);
+});
 
 module.exports = router;
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
-var indexController = require('../controllers/indexController');
+var ingredientModel = require('../models/ingredientModel');
 
 // Get Home Page
-router.get('/', indexController.homePage);
+router.get('/', function(req, res) {
+  // get data from mongodb and pass it to the view
+  ingredientModel.find({}, function(err, data) {
+      if (err) throw err;
+      res.render('index', {
+          ingredientsList: ingredientsList,
+          recipes: data
+      });
+  });
+});
 
 // Delete recipe
-router.delete('/recipe/delete/:recipeID', indexController.deleteRecipe);
+router.delete('/recipe/delete/:recipeID', function(req, res) {
+  var recipeID = req.params.recipeID;
+  ingredientModel.remove({_id: recipeID}, function(err, data) {
+      if (err) {
+        res.status(500).send(err);
+      };
+  });
+  res.send(ingredientsList);
+});
 
 module.exports = router;
