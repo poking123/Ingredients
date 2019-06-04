6c39f32b28639d3c0b0772be5ad4bd832aec7b43
Brian Gao <bgao@brandeis.edu>
2019-06-03 20:04:46 -0400

    Front end set up with heroku-postbuild

--- routers/api/recipes.js
+++ routers/api/recipes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Recipe model
-const Recipe = require('../../models');
+const Recipe = require('../../models/RecipeModel');
 
 // @route   GET api/recipes
 // @desc    Get All Recipes
@@ -11,3 +11,26 @@ router.get('/', (req, res) => {
     Recipe.find()
         .then(items => res.json(items))
 });
+
+// @route   POST api/recipes
+// @desc    Create a Recipe
+// @access  Public
+router.post('/', (req, res) => {
+    const newRecipe = new Recipe({
+        name: req.body.name,
+        ingredients: req.body.ingredients
+    });
+
+    newRecipe.save().then(recipe => res.json(recipe));
+});
+
+// @route   DELETE api/recipes/:id
+// @desc    Delete a Recipe
+// @access  Public
+router.post('/', (req, res) => {
+    Item.findById(req.params.id)
+    .then(recipe => recipe.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const recipes = require('./routers/api/recipes');
+
 
 var app = express();
 
 // connect to the database
 const db = keys.mongoURI;
-mongoose
-    .connect(db)
+mongoose.connect(db, { useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
 // Require all the routers
-var indexRouter = require('./routers/indexRouter');
-var addRecipeRouter = require('./routers/addRecipeRouter');
-var editRecipeRouter = require('./routers/editRecipeRouter');
+// var indexRouter = require('./routers/indexRouter');
+// var addRecipeRouter = require('./routers/addRecipeRouter');
+// var editRecipeRouter = require('./routers/editRecipeRouter');
 
 // Body-parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,11 +26,21 @@ app.set('view engine', 'ejs');
 app.use(express.static('./assets'));
 
 // Use all the routers in the app
-app.use('/', indexRouter);
-app.use('/Add_Recipe', addRecipeRouter);
-app.use('/Edit_Recipe', editRecipeRouter);
+// app.use('/', indexRouter);
+// app.use('/Add_Recipe', addRecipeRouter);
+// app.use('/Edit_Recipe', editRecipeRouter);
 
 app.use('/api/recipes', recipes);
+
+// Serve Static Assets if in Production
+if (process.env.NODE_ENV === 'production') {
+    // Set Static Folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
