9773ee2517f3e4072acb631fcf8d252c16759242
Brian Gao <bgao@brandeis.edu>
2019-11-30 15:48:49 -0500

    added some mocha tests

--- test/tests.js
+++ test/tests.js
@@ -1,56 +1,87 @@
-var mocha = require('mocha');
-var assert = require('assert');
-var ingredientModel = require('../models/ingredientModel');
+let mongoose = require('mongoose');
+const path = require('path');
+let assert = require('assert');
+let Recipe = require('../models/RecipeModel');
+
+
+before(function(done) {
+    const envPath = path.join(__dirname, '..', '.env.production.local');
+
+    require('dotenv').config({path: envPath});
+
+    const db = process.env.MONGO_URI;
+
+    var settings = {
+        reconnectTries : 10,
+        autoReconnect : true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    };
+    
+    mongoose.connect(db, settings)
+        .then(() => {
+            console.log('MongoDB Connected...');
+            done();
+        })
+        .catch(err => {
+            console.log('MongoDB Not Connected!!!');
+            console.log(err);
+        });
+});
 
 describe('Unit Tests for ingredients', function() {
     
-    
     it('Add a Recipe to the database', function(done) {
-        var list = [];
-        var tomatoes = {
+        let tomatoes = {
            name: 'Tomatoes',
            quantity: 2
         };
 
-        var bacon = {
+        let bacon = {
             name: 'Bacon',
             quantity: 9
         };
 
-        list.push(tomatoes);
-        list.push(bacon);
-        var recipe = new ingredientModel({
-            name: "RecipeName",
-            ingredients: list
+        let ingredients = [tomatoes, bacon];
+
+        let clientId = 'dklfjgdljgfdlvlkvldl';
+
+        var recipe = new Recipe({
+            name: "AddRecipeTest",
+            ingredients,
+            clientId
         });
 
         recipe.save().then(function() {
-            assert(recipe.isNew === false);
+            assert(recipe.isNew === false); // false once it's been saved to the db
             done();
         });
     });
     
     it('Delete a Recipe from the database', function(done) {
-        var list = [];
-        var tomatoes = {
-           name: 'Tomatoes',
-           quantity: 2
-        };
+        let tomatoes = {
+            name: 'Tomatoes',
+            quantity: 2
+         };
+ 
+         let bacon = {
+             name: 'Bacon',
+             quantity: 9
+         };
 
-        var bacon = {
-            name: 'Bacon',
-            quantity: 9
-        };
-
-        list.push(tomatoes);
-        list.push(bacon);
-        var recipe = new ingredientModel({
-            name: "RecipeName",
-            ingredients: list
-        });
+         let clientId = 'dklfjgdljgfdlvlkvldl';
+ 
+         let ingredients = [tomatoes, bacon];
+         var recipe = new Recipe({
+             name: "DeleteRecipeTest",
+             ingredients,
+             clientId
+         });
 
         recipe.save().then(function() {
-            ingredientModel.deleteOne({recipe}, function(result) {
+            Recipe.deleteOne({recipe}, function(result) {
                 assert(result === null);
                 done();
             });
